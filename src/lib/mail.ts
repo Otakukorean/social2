e7e10522f74a4023571d9c314d7b4d75034a6c19
Hotturtle1 <ahmadmustafa287@yahoo.com>
2024-06-13 20:02:38 +0300

    lo

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -3,7 +3,7 @@ import {Resend} from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async (email : string,token : string) => {
-    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const resetLink = `https://main--sweet-beignet-afcb53.netlify.app/auth/new-password?token=${token}`;
     await resend.emails.send({
         from : "no-replay@email.redanime.net" , 
         to : email,
@@ -13,7 +13,7 @@ export const sendPasswordResetEmail = async (email : string,token : string) => {
 }
 
 export const sendVerficationEmail = async (email : string,token : string) => {
-        const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
+        const confirmLink = `https://main--sweet-beignet-afcb53.netlify.app/auth/new-verification?token=${token}`;
         await resend.emails.send({
             from : "no-replay@email.redanime.net" , 
             to : email,
